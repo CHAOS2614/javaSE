aafb2f4f391d37d6e6881c6ea9a67a4b7d148c50
CHAOS2614 <330276680@qq.com>
2020-11-04 11:12:47 +0800

    增删改通用操作及测试

--- JDBC/src/cn/edu/bjfu/preparedstatement/PreparedStatementTest.java
+++ JDBC/src/cn/edu/bjfu/preparedstatement/PreparedStatementTest.java
@@ -16,6 +16,29 @@ import java.text.SimpleDateFormat;
  */
 public class PreparedStatementTest {
 
+    /**
+     * 增删改通用操作
+     * @param sql 带占位符的sql语句
+     * @param args 填充占位符的可变形参，长度等于sql中占位符个数
+     */
+    public void update(String sql, Object... args) {
+
+        Connection connection = Utils.getConnection();
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Utils.closeResource(connection, preparedStatement);
+        }
+    }
+
     @Test
     public void insertTest() {
 
@@ -31,8 +54,7 @@ public class PreparedStatementTest {
             preparedStatement.setString(1, "哪吒");
             preparedStatement.setString(2, "nezha@gmail.com");
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyy-MM-dd");
-            java.util.Date date = null;
-            date = simpleDateFormat.parse("1000-01-01");
+            java.util.Date date = simpleDateFormat.parse("1000-01-01");
             if (date != null) {
                 preparedStatement.setDate(3, new Date(date.getTime()));
             }
@@ -70,4 +92,25 @@ public class PreparedStatementTest {
             Utils.closeResource(connection, preparedStatement);
         }
     }
+
+    @Test
+    public void commonUpdateDeleteTest(){
+        String sql = "delete from customers where id = ?";
+        update(sql,21);
+    }
+
+    @Test
+    public void commonUpdateInsertTest(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyy-MM-dd");
+        java.util.Date date = null;
+        try {
+             date = simpleDateFormat.parse("1000-01-01");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //order表名是关键字，要加``，否则报错
+        String sql = "insert into `order`(order_name,order_date)values(?,?)";
+        update(sql,"DD",date);
+    }
+
 }
