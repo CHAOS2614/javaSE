e8a2aa2053762d5d49d376746aceabc9b14e3545
CHAOS2614 <330276680@qq.com>
2020-11-05 19:28:42 +0800

    blob插入与查询操作

---
+++ JDBC/src/cn/edu/bjfu/blob/BlobTest.java
@@ -0,0 +1,92 @@
+package cn.edu.bjfu.blob;
+
+import cn.edu.bjfu.Utils;
+import cn.edu.bjfu.javabean.Customer;
+import cn.edu.bjfu.preparedstatement.PreparedStatementQueryTest;
+import cn.edu.bjfu.preparedstatement.PreparedStatementTest;
+import org.junit.Test;
+
+import java.io.*;
+import java.sql.*;
+
+/**
+ * @author Chao Huaiyu
+ * @date 2020/11/5
+ */
+public class BlobTest {
+
+    @Test
+    public void insertTest() {
+        String sql = "insert into customers(name,email,birth,photo)values(?,?,?,?)";
+
+        String name = "美铃遥";
+        String email = "love@fragtime.com";
+        String birth = "2019-11-22";
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream("images\\fragile.JPG");
+            PreparedStatementTest pst = new PreparedStatementTest();
+            pst.update(sql, name, email, birth, fileInputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void queryTest() {
+        String sql = "select id,name,email,birth from customers where id =?";
+        PreparedStatementQueryTest psqt = new PreparedStatementQueryTest();
+        Customer customer = psqt.getInstance(Customer.class, sql, 20);
+        System.out.println(customer);
+
+        Connection connection = Utils.getConnection();
+        String sql2 = "select photo from customers where id = ?";
+        InputStream binaryStream = null;
+        FileOutputStream fos = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = connection.prepareStatement(sql2);
+            ps.setInt(1, 20);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+
+                Blob photo = rs.getBlob("photo");
+                binaryStream = photo.getBinaryStream();
+                fos = new FileOutputStream("images\\fragile2.jpg");
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = binaryStream.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len);
+                }
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (binaryStream != null) {
+                try {
+                    binaryStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Utils.closeResource(connection, ps, rs);
+        }
+    }
+}
