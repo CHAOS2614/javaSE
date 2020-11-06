53490df4bc464358c0e9a6c05323fc838efb759a
CHAOS2614 <330276680@qq.com>
2020-11-06 22:54:22 +0800

    DBUtils工具类,MapHandler和MapListHandler简单示例

--- JDBC/src/cn/edu/bjfu/dbutils/DbUtilsTest.java
+++ JDBC/src/cn/edu/bjfu/dbutils/DbUtilsTest.java
@@ -5,12 +5,15 @@ import cn.edu.bjfu.javabean.Customer;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * commons-dbutils 是Apache 组织提供的一个开源的JDBC工具类库
@@ -78,4 +81,51 @@ public class DbUtilsTest {
             Utils.closeResource(connection, null);
         }
     }
+
+    /**
+     * MapHandler:是ResultSetHandler接口的实现类
+     * 对应表中的一条记录
+     * 将字段及相应的值作为map中的key和value
+     */
+    @Test
+    public void queryTest3() {
+        Connection connection = null;
+        try {
+            connection = Utils.getConnection();
+            String sql = "select id,name,email,birth from customers where id = ?";
+            QueryRunner runner = new QueryRunner();
+            MapHandler handler = new MapHandler();
+            Map<String,Object> map = runner.query(connection, sql, handler, 10);
+            System.out.println(map);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Utils.closeResource(connection, null);
+        }
+    }
+
+    /**
+     * MapListHandler:是ResultSetHandler接口的实现类
+     * 对应表中的多条记录
+     * 将字段及相应的值作为map中的key和value
+     * 构成map对象构成的list
+     */
+    @Test
+    public void queryTest4() {
+        Connection connection = null;
+        try {
+            connection = Utils.getConnection();
+            String sql = "select id,name,email,birth from customers where id > ?";
+            QueryRunner runner = new QueryRunner();
+            MapListHandler handler = new MapListHandler();
+            List<Map<String, Object>> list = runner.query(connection, sql, handler, 10);
+            list.forEach(System.out::println);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Utils.closeResource(connection, null);
+        }
+    }
+
+
 }
